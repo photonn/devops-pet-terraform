6765241a329708a2b3b2a1395e1d5205fe252891
Miguel Martin <miguel_martin@epam.com>
2019-10-26 10:27:23 +0200

    notes

--- jenkins.tf
+++ jenkins.tf
@@ -24,11 +24,13 @@ resource "kubernetes_pod" "jenkins_pod" {
       }
     }
     # if a bootstraping variable is set to true, after mapping local volume, bootstrap with config 
-    # from git repo (map volume git_repo type and exec a copy). If false, just map local volume.
-    #exec
-    #Arguments
-    #command - (Optional) Command is the command line to execute inside the container, the working directory for the command is root ('/') in the container's filesystem. The command is simply exec'd, it is not run inside a shell, so traditional shell instructions. To use a shell, you need to explicitly call out to that shell. Exit status of 0 is treated as live/healthy and non-zero is unhealthy.
-
+    # from git repo (map volume git_repo type and exec a copy jenkins home). If false, just map local volume.
+    # exec
+    # Arguments
+    # command - (Optional) Command is the command line to execute inside the container, the working directory for the command is root ('/') in the container's filesystem. The command is simply exec'd, it is not run inside a shell, so traditional shell instructions. To use a shell, you need to explicitly call out to that shell. Exit status of 0 is treated as live/healthy and non-zero is unhealthy.
+    # use powershell script to check environment see below
+    # check if exists and mount this route as a local volume /host_mnt/c/jenkins_local_data to /var/jenkins_home
+    # 
   }
 }
 
