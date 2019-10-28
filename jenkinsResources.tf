41259c82328531e23f3fdf980256db5e9dc0b553
Miguel Martin <miguel_martin@epam.com>
2019-10-28 16:33:11 +0100

    added port 50000 for jnpl to service

--- jenkinsResources.tf
+++ jenkinsResources.tf
@@ -7,11 +7,11 @@ resource "kubernetes_pod" "jenkins_pod" {
     }
   }
   spec {
-    volume{
+    volume {
       name = "jenkins-volume"
-      persistent_volume_claim{
+      persistent_volume_claim {
         claim_name = "jenkins-volume-claim"
-      } 
+      }
     }
     container {
       image = "${var.jenkins_image}"
@@ -23,28 +23,37 @@ resource "kubernetes_pod" "jenkins_pod" {
       port {
         container_port = 8080
       }
+      port {
+        container_port = 50000
+      }
     }
   }
 }
 
 
-resource "kubernetes_service" "jenkins-service" {
+resource "kubernetes_service" "jenkins-service-8080" {
   metadata {
-    name      = "jenkins-service"
+    name      = "jenkins-service-8080"
     namespace = "devops-pet-project"
   }
   spec {
     selector = {
       App = "jenkins"
     }
-    port {
-      port        = 8080
+    port{
+      name = "jekinsport"
+      port = 8080
       target_port = 8080
+    }
+    port{
+      name = "jnplport"
+      port = 50000
+      target_port = 50000
     }
     type = "NodePort"
   }
 }
 
 output "connection_point" {
-  value = "http://localhost:${kubernetes_service.jenkins-service.spec[0].port[0].node_port}"
+  value = "http://localhost:${kubernetes_service.jenkins-service-8080.spec[0].port[0].node_port}"
 }
--- jenkinsVolumes.tf
+++ jenkinsVolumes.tf
@@ -1,10 +1,10 @@
 resource "kubernetes_persistent_volume_claim" "jenkins-volume-claim" {
   metadata {
-    name = "jenkins-volume-claim"
+    name      = "jenkins-volume-claim"
     namespace = "devops-pet-project"
   }
   spec {
-    access_modes = ["ReadWriteMany"]
+    access_modes       = ["ReadWriteMany"]
     storage_class_name = "local-storage-class"
     resources {
       requests = {
@@ -23,7 +23,7 @@ resource "kubernetes_persistent_volume" "jenkins-volume" {
     capacity = {
       storage = "4Gi"
     }
-    access_modes = ["ReadWriteMany"]
+    access_modes       = ["ReadWriteMany"]
     storage_class_name = "local-storage-class"
     persistent_volume_source {
       host_path {
